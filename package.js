ded80413bc36d4d10f9601bf26c2bc3d507da4c5
Yacine Merzouk <yacine@merzouk.ca>
2021-09-18 15:30:28 -1000

    Updated for Meteor 2.3.6

--- package.js
+++ package.js
@@ -1,30 +1,30 @@
 Package.describe({
   name: 'meteorhubdotnet:firestorm-iron-middleware-stack',
   summary: 'Fork of iron:middleware-stack for Firestorm',
-  version: '1.0.4',
+  version: '1.0.5',
   git: 'https://github.com/meteorhubdotnet/firestorm-iron-middleware-stack'
 });
 
-Package.on_use(function (api) {
-  api.versionsFrom('METEOR@2.2');
+Package.onUse(function (api) {
+  api.versionsFrom('METEOR@2.3.6');
 
   api.use('underscore');
   api.use('ejson');
 
-  api.use('meteorhubdotnet:firestorm-iron-core@1.0.4');
-  api.imply('meteorhubdotnet:firestorm-iron-core@1.0.4');
+  api.use('meteorhubdotnet:firestorm-iron-core@1.0.5');
+  api.imply('meteorhubdotnet:firestorm-iron-core@1.0.5');
 
-  api.use('meteorhubdotnet:firestorm-iron-url@1.0.4');
+  api.use('meteorhubdotnet:firestorm-iron-url@1.0.5');
 
-  api.add_files('lib/handler.js');
-  api.add_files('lib/middleware_stack.js');
+  api.addFiles('lib/handler.js');
+  api.addFiles('lib/middleware_stack.js');
   api.export('Handler', {testOnly: true});
 });
 
-Package.on_test(function (api) {
+Package.onTest(function (api) {
   api.use('meteorhubdotnet:firestorm-iron-middleware-stack');
   api.use('tinytest');
   api.use('test-helpers');
-  api.add_files('test/handler_test.js');
-  api.add_files('test/middleware_stack_test.js');
+  api.addFiles('test/handler_test.js');
+  api.addFiles('test/middleware_stack_test.js');
 });
