90efadde830db14ef8ed5d11735023a0ee62fdb9
Yacine Merzouk <yacine@merzouk.ca>
2022-08-20 21:28:17 -0400

    1.0.7 Updated package dependencies

--- package.js
+++ package.js
@@ -1,20 +1,20 @@
 Package.describe({
   name: 'meteorhubdotnet:firestorm-iron-middleware-stack',
   summary: 'Fork of iron:middleware-stack for Firestorm',
-  version: '1.0.5',
+  version: '1.0.7',
   git: 'https://github.com/meteorhubdotnet/firestorm-iron-middleware-stack'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@2.3.6');
+  api.versionsFrom('METEOR@2.7.3');
 
   api.use('underscore');
   api.use('ejson');
 
-  api.use('meteorhubdotnet:firestorm-iron-core@1.0.5');
-  api.imply('meteorhubdotnet:firestorm-iron-core@1.0.5');
+  api.use('meteorhubdotnet:firestorm-iron-core@1.0.7');
+  api.imply('meteorhubdotnet:firestorm-iron-core@1.0.7');
 
-  api.use('meteorhubdotnet:firestorm-iron-url@1.0.5');
+  api.use('meteorhubdotnet:firestorm-iron-url@1.0.7');
 
   api.addFiles('lib/handler.js');
   api.addFiles('lib/middleware_stack.js');
